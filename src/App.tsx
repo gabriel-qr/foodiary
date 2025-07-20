cfd8a630ef63e978349fc5027cc78ea7fc0b347e
gabriel-qr <gabriel.qr.dev@gmail.com>
2025-07-20 10:42:37 -0300

    DateSwitcher and HomeHeader Components

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import {
   useFonts,
 } from '@expo-google-fonts/host-grotesk';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import { useEffect } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { HomeHeader } from './components/HomeHeader';
 import './styles/global.css';
+import { DateSwitcher } from './components/DateSwitcher';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -33,9 +35,11 @@ export default function App() {
   }
 
   return (
-    <View className='bg-lime-500 flex-1 items-center justify-center'>
-      <Text className='text-base'>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
+    <View className='flex-1 bg-white'>
+      <SafeAreaProvider>
+        <HomeHeader />
+        <DateSwitcher />
+      </SafeAreaProvider>
     </View>
   );
 }
