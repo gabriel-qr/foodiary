16efef756d4bdcea4bd2184cbaa63351e75bd3cc
gabriel-qr <gabriel.qr.dev@gmail.com>
2025-07-20 11:11:42 -0300

    Meals and DailyStats

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,11 @@ import { View } from 'react-native';
 
 import { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { HomeHeader } from './components/HomeHeader';
-import './styles/global.css';
+import { DailyStats } from './components/DailyStats';
 import { DateSwitcher } from './components/DateSwitcher';
+import { HomeHeader } from './components/HomeHeader';
+import { MealsList } from './components/MealsList';
+import './styles/global.css';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -39,6 +41,30 @@ export default function App() {
       <SafeAreaProvider>
         <HomeHeader />
         <DateSwitcher />
+        <View className='mt-2'>
+          <DailyStats
+            calories={{
+              current: 500,
+              goal: 2500,
+            }}
+            proteins={{
+              current: 2000,
+              goal: 2500,
+            }}
+            carbohydrates={{
+              current: 500,
+              goal: 2500,
+            }}
+            fats={{
+              current: 500,
+              goal: 2500,
+            }}
+          />
+        </View>
+
+        <View className='h-px bg-gray-200 mt-7' />
+
+        <MealsList />
       </SafeAreaProvider>
     </View>
   );
